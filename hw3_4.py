e7a20f19b425bb52e3aa992f38ce03183e259555
Chet Lemon <clemon@ucsd.edu>
2015-05-01 02:26:52 -0700

    fukboi

--- hw3_4.py
+++ hw3_4.py
@@ -17,9 +17,10 @@ class Node:
 		# data: list of Flower objects in the node
 		# label: set to None for non-leaf nodes
 		# left/right: left/right child (Node obj)
-	def __init__(self, data, label, left, right):
+	def __init__(self, data, label, rule, left, right):
 		self.data = data
 		self.label = label
+		self.rule = rule
 		self.left = left
 		self.right = right
 		
@@ -35,6 +36,7 @@ with open("hw3train.txt") as f:
 # Calculates entropy given a list of flower objects
 # jizzes out a float
 def calcEntropy(flowers):
+	print flowers
 	labelCounts = [0,0,0]
 	for flower in flowers:
 		labelCounts[int(flower.label-1)] += 1
@@ -54,8 +56,6 @@ def calcEntropy(flowers):
 		
 	return entropy
 
-root = buildTree(train)
-
 # isPure
 # checks if a node is pure (contains only one label)
 def isPure(data):
@@ -63,33 +63,56 @@ def isPure(data):
 	for flower in data:
 		labelCounts[int(flower.label-1)] += 1
 		
-	if labelCounts[0] == len(data) || labelCounts[1] == len(data) || labelCounts[2] == len(data):
+	if labelCounts[0] == len(data) or labelCounts[1] == len(data) or labelCounts[2] == len(data):
 		return True
 	else:
 		return False
 
 # splitData
 # input: sum dahta [biglist]
-# out: [[list1],[list2]]
+# out: (feature, thresh, entropy, list1, list2)
 def splitData(data):
-	flists = []
-	optimalSplit = None
+	flists = [[],[],[],[]]
+	optimalSplit = None		# (feature, thresh, entropy, list1, list2)
 	# loop through the features
 	for feature in range(4):
 		flists[feature] = sorted(data, key=lambda x: x.vector[feature], reverse=True)	# sort data by feature
 		# loop through each point
+		prevPoint = None
 		for point in flists[feature]:
-			
+			if prevPoint != None:
+				threshold = abs(point.vector[feature] - prevPoint.vector[feature]) / 2.0
+				
+				# split list
+				list1 = []
+				list2 = []
+				for thingy in flists[feature]:
+					if thingy.vector[feature] < threshold:
+						list1.append(thingy)
+					else:
+						list2.append(thingy)
+				print list1, list2
+				entropy = calcEntropy(list1) + calcEntropy(list2)
+				if optimalSplit == None or entropy < optimalSplit[2]:
+					optimalSplit = (feature, threshold, entropy, list1, list2)
+				
 			prevPoint = point
+			
+	return optimalSplit
 	
 # buildTree: recursive
 # pass the data and a tree shall rise
 def buildTree(data):
 	if isPure(data): 
-		return Node(data, data[0].label, None, None)
+		return Node(data, data[0].label, None, None, None)
 	else:
 		# split data
+		dopeSlicings = splitData(data)
 		# recurse left
+		leftNode = buildTree(dopeSlicings[3])
 		# recurse right
-		# check the op
+		rightNode = buildTree(dopeSlicings[4])
+		
+		return Node(None, None, dopeSlicings, leftNode, rightNode)
 
+print buildTree(train)
