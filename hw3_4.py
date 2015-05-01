87fc60d715da5db2a0c3455dbaea7820d7c80717
Chet Lemon <clemon@ucsd.edu>
2015-04-30 23:14:25 -0700

    added method to calc entropy

--- hw3_4.py
+++ hw3_4.py
@@ -5,6 +5,7 @@
 # Kesav Mulakaluri (A10616114) #
 # Chris Zelazo (A10863450)     #
 ################################
+import math
 
 class Flower:
 	def __init__(self, vector, label):
@@ -19,5 +20,16 @@ with open("hw3train.txt") as f:
 	train = [map(float, line.rstrip().split(' ')) for line in f]
 	train = [Flower(l[:4], l[4]) for l in train]
 
-print train[4].vector
-print train[4].label
+# Calculates entropy given a list of flower objects
+# jizzes out a float
+def calcEntropy(flowers):
+	labelCounts = [0,0,0]
+	for flower in flowers:
+		labelCounts[int(flower.label-1)]
+		
+	p1 = labelCounts[0] / float(len(flowers))  	# P(X=1)
+	p2 = labelCounts[1] / float(len(flowers))	# P(X=2)
+	p3 = labelCounts[2] / float(len(flowers))	# P(X=3)
+	
+	# entropy eqn
+	return -(p1*math.log(p1)) - (p2*math.log(p2)) - (p2*math.log(p3))
